c5f498a8b94ca75baeddf09015d37c2c6eae5882
dongngotngao985@gmail.com <dongngotngao985@gmail.com>
2023-04-03 14:33:19 +0700

    chore: log error

--- android/src/main/java/com/fantastic/manage_calendar_events/ManageCalendarEventsPlugin.java
+++ android/src/main/java/com/fantastic/manage_calendar_events/ManageCalendarEventsPlugin.java
@@ -129,6 +129,7 @@ public class ManageCalendarEventsPlugin implements FlutterPlugin, ActivityAware,
             CalendarEvent event = new CalendarEvent(eventId, title, description, startDate,
                     endDate, location, url, isAllDay, hasAlarm);
             operations.createUpdateEvent(calendarId, event);
+            Log.d("attendees", call.hasArgument("attendees"));
             if (call.hasArgument("attendees")) {
                 addAttendees(event.getEventId(), call);
             }
