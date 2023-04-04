f9faef86e908a13fc6b20607adb0c88205ee886a
dongngotngao985@gmail.com <dongngotngao985@gmail.com>
2023-04-05 01:17:41 +0700

    refactor: add part library into manage calendar file

--- lib/manage_calendar_events.dart
+++ lib/manage_calendar_events.dart
@@ -1,16 +1,17 @@
 library manage_calendar_events;
 
-// Dart imports:
 import 'dart:async';
 import 'dart:convert';
 
-// Flutter imports:
 import 'package:flutter/services.dart';
-
-// Project imports:
+import 'package:hive/hive.dart';
 import 'package:manage_calendar_events/src/constants/constants.dart';
-import 'package:manage_calendar_events/src/model/local_calendar_model.dart';
+import 'package:manage_calendar_events/src/constants/local_key.dart';
+import 'package:path_provider/path_provider.dart';
 
 part 'src/calendar_plugin.dart';
-part 'src/model/calendar.dart';
-part 'src/model/calendar_event.dart';
+part 'src/models/calendar.dart';
+part 'src/models/calendar_event.dart';
+part 'src/repository/local_data/calendar_events_local.dart';
+part 'src/models/calendar_local_model.dart';
+part 'src/repository/local_data/base_local_repository.dart';
